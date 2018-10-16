6fef832abbef8a6a051303c847d3df1eb95e5647
Bruut8 <ruudabma@gmail.com>
2018-10-16 10:13:20 +0000

    Working, updated comments and spelling

--- updatefeed.py
+++ updatefeed.py
@@ -16,6 +16,8 @@ NAMESPACES = {
 	'dc': 'http://purl.org/dc/elements/1.1/'
 }
 
+
+
 # functions
 def getwebcontent(url):
 	#print(' Opening url: ' + url)
@@ -44,23 +46,45 @@ def cleanxml(s):
 	return s
 
 def parsexml(xml, name, feed):
-	print(' Attempting to drop table for journal ' + name)
-	cur.execute('DROP TABLE IF EXISTS ' + name)
-	print('  Succes!')
-	print(' Attempting to create database for journal ' + name)
-	sql_cmd = 'CREATE TABLE IF NOT EXISTS ' + name + '(title VARCHAR(400), authors VARCHAR(500), pmid INT(8), doi VARCHAR(30), volume VARCHAR(4), issue VARCHAR(3), pubdate DATE, pubtype VARCHAR(500), abstract VARCHAR(3000), pdflink VARCHAR(500))'
-	cur.execute(sql_cmd)
-	print('  Succes!')
 	if 'ovidsp.ovid.com' in feed:
 		parseovid(xml, name)
 	elif 'wiley.com' in feed:
 		parsewiley(xml, name)
 	return
 
+def resettable(name):
+	print(' Attempting to drop table for journal ' + name)
+	cur.execute('DROP TABLE IF EXISTS ' + name)
+	print('  Succes!')
+	print(' Attempting to create table for journal ' + name)
+	sql_cmd = 'CREATE TABLE IF NOT EXISTS ' + name + '(title VARCHAR(400), authors VARCHAR(500), pmid INT(8), doi VARCHAR(30), volume VARCHAR(4), issue VARCHAR(3), pubdate DATE, pubtype VARCHAR(500), abstract VARCHAR(3000), pdflink VARCHAR(500), weblink VARCHAR(500))'
+	cur.execute(sql_cmd)
+	print('  Succes!')
+	
 def parsewiley(xml, name):
-	pmidlist = []
-	print(' Identified Wiley-type feed, pubmed doi search required..')
 	xdoc = loadxml(xml)
+	pmidlist = []
+	# Get vol and issue from xml
+	t = re.search('Volume\s+(\d+).*Issue\s+(\d+)',cleanxml(str(xdoc.xpath('//rdf:RDF/rss:item/rss:description/text()', namespaces=NAMESPACES)))) 
+	volume = t.group(1)
+	issue = t.group(2)
+	print(' XML version: Volume ' + volume + ' Issue ' + issue)
+	# Get vol and issue from database
+	cur.execute('SELECT volume,issue FROM shelf WHERE journal = %s', (name,))
+	rows = cur.fetchall()
+	for r in rows:
+		voldb = str(r[0])
+		issdb = str(r[1])
+		print(name + ' last updated to volume ' + voldb + ' issue ' + issdb)
+	# compare and if match then skip
+	if volume == voldb and issue == issdb:
+		print(' Already up to date, skipping ' + name)
+		return	
+	
+	# if not skipped then drop table
+	
+	print(' Identified Wiley-type feed, pubmed doi search required..')
+	resettable(name)
 	for i in xdoc.xpath('//rdf:RDF/rss:item', namespaces=NAMESPACES):
 		doi = re.sub(r'doi:', '', str(i.xpath('dc:identifier/text()', namespaces=NAMESPACES)[0]))
 		pmid = pmtermsearch(doi, 'doi')
@@ -70,6 +94,9 @@ def parsewiley(xml, name):
 			pmidlist.append(pmid)
 	if len(pmidlist) > 0:
 		pmidtodb(pmidlist, name)
+		print(' Updating shelf...')
+		# Update shelf
+		cur.execute('UPDATE shelf SET volume = %s, issue = %s WHERE journal = %s',(volume, issue, name))
 	else:
 		print(' No PMID\'s found in entire feed. Feed correct?')
 	return
@@ -77,23 +104,48 @@ def parsewiley(xml, name):
 def parseovid(xml, name):
 	xdoc = loadxml(xml)
 	pmidlist = []
+	# Get vol and issue from xml
+	t = re.search('Volume\s+(\d+)\((\d+)\)',cleanxml(str(xdoc.xpath('//channel/title/text()')))) 
+	volume = t.group(1)
+	issue = t.group(2)
+	print(' XML version: Volume ' + volume + ' Issue ' + issue)
+	# Get vol and issue from database
+	cur.execute('SELECT volume,issue FROM shelf WHERE journal = %s', (name,))
+	rows = cur.fetchall()
+	for r in rows:
+		voldb = str(r[0])
+		issdb = str(r[1])
+		print(name + ' last updated to volume ' + voldb + ' issue ' + issdb)
+	# compare and if match then skip
+	if volume == voldb and issue == issdb:
+		print(' Already up to date, skipping ' + name)
+		return	
+	
+	# if not skipped then drop table	
+	resettable(name)
 	print(' Identified OVID-type feed, pubmed title search required..')
+	# Get titles from xml 
 	for i in xdoc.xpath('//channel/item'):
 		at = cleanxml(str(i.xpath('title/text()')[0]))
+		# Convert titles to pmids
 		pmid = pmtermsearch(at, 'title')
 		if pmid == 0:
 			print('  Skipping entry..')
 		else:
 			pmidlist.append(pmid)
+	# If titles are converted to pmids
 	if len(pmidlist) > 0:
 		pmidtodb(pmidlist, name)
+		print(' Updating shelf...')
+		# Update shelf
+		cur.execute('UPDATE shelf SET volume = %s, issue = %s WHERE journal = %s',(volume, issue, name))
 	else:
 		print(' No PMID\'s found in entire feed. Feed correct?')
 	return
 
 def pmtermsearch(term, type):
 	print(' Searching PubMed by ' + type + ': ' + term)
-	term = re.sub(r'\s', '+', term)
+	term = re.sub(r'\s', '+', term)	
 	url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=' + term + '&retmode=xml&field=' + type + '&reldate=300'
 	xml = getwebcontent(url)
 	xdoc = loadxml(xml)
@@ -129,6 +181,8 @@ def pmidtodb(pmidlist, dbname):
 			for a in i.xpath('MedlineCitation/Article/Abstract/AbstractText'):
 				abstract.append(str(a.xpath('text()')[0]))
 			abstract = '\n'.join(abstract)
+			if not abstract:
+				abstract = "No abstract available"
 			pubtype = []
 			for a in i.xpath('MedlineCitation/Article/PublicationTypeList/PublicationType'):
 				pubtype.append(str(a.xpath('text()')[0]))
@@ -136,21 +190,25 @@ def pmidtodb(pmidlist, dbname):
 			#print(name,volume,issue,pubdate,pubtype,authors,pmid,doi,abstract,sep='\n')
 			doired = getredirect('https://doi.org/' + doi)
 			if 'Insights.ovid.com' in doired:
-				#ovid weblink
+				#ovid pdflink
 				an = re.search(r'an=(.*)', doired)
 				# pdflink = 'http://ovidsp.ovid.com/ovidweb.cgi?T=JS&CSC=Y&NEWS=N&PAGE=fulltext&AN=' + an.group(1) + '&LSLINK=80&D=ovft&CHANNEL=PubMed&PDF=y' 
 				# Radboud:
 				pdflink = 'http://ovidsp.tx.ovid.com.ru.idm.oclc.org/sp-3.31.1a/ovidweb.cgi?T=JS&CSC=Y&NEWS=N&PAGE=fulltext&AN=' + an.group(1) + '&LSLINK=80&D=ovft&CHANNEL=PubMed&PDF=y'
+				#ovid weblink
+				weblink = 'http://ovidsp.tx.ovid.com.ru.idm.oclc.org/sp-3.31.1a/ovidweb.cgi?T=JS&CSC=Y&NEWS=N&PAGE=fulltext&AN=' + an.group(1) + '&LSLINK=80&D=ovft&CHANNEL=PubMed&PDF=n'
 			elif 'wiley.com' in doired:
-				#wiley weblink
+				#wiley pdflink
 				pdflink = 'https://onlinelibrary-wiley-com.ru.idm.oclc.org/doi/epdf/' + doi
+				#wiley weblink
+				weblink = 'https://onlinelibrary-wiley-com.ru.idm.oclc.org/doi/full/' + doi
 			else:
 				pdflink = 0
-				print(' Geen PDF link kunne verkrijgen!')
+				print(' Geen PDF link kunnen verkrijgen!')
 			print(' Adding ' + pmid + ' to database...')
 			#sql_cmd = 'INSERT INTO ' + dbname + 'VALUES(' + title + ',' + authors + ',' + pmid + ',' + doi + ',' + volume + ',' + issue + ',' + pubdate + ',' + pubtype + ',' + abstract + ',' + pdflink + ')'
 			try:
-				cur.execute('INSERT INTO ' + dbname + ' (title, authors, pmid, doi, volume, issue, pubdate, pubtype, abstract, pdflink) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(title,authors,pmid,doi,volume,issue,pubdate,pubtype,abstract,pdflink))
+				cur.execute('INSERT INTO ' + dbname + ' (title, authors, pmid, doi, volume, issue, pubdate, pubtype, abstract, pdflink, weblink) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',(title,authors,pmid,doi,volume,issue,pubdate,pubtype,abstract,pdflink,weblink))
 				print('  Succes!')
 			except MySQLdb.Error as e:
 				print(' Error: %d, %s' % (e.args[0],e.args[1]))
@@ -187,4 +245,7 @@ exit()
 
 
 # table
-# title, authors, pmid, doi, volume, issue, pubdate, pubtype, abstract, pdflink
+# title, authors, pmid, doi, volume, issue, pubdate, pubtype, abstract, pdflink, weblink
+
+# shelf table
+# journal, feed, last_update, volume, issue
